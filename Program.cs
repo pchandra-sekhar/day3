fbf1b73704ab161c234b12e2f43ff1fcba367d27
pchandra-sekhar <pchandrasekhar7777@gmail.com>
2025-01-29 12:23:43 +0530

    Added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class person
+{
+    public string name=Console.ReadLine();
+
+    public void Display()
+    {
+        Console.WriteLine($"Hello my name is {name}");
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        person p=new person();
+
+       // p.name = "Chandrasekhar";
+        p.Display();
+
+
+    }
+}
